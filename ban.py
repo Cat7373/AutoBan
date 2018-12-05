e8dcd325a6a9f92a9afb2353951ba2abf789d6d0
Cat73 <root@cat73.org>
2018-12-05 19:43:21 +0800

    :bug: 修复写出的规则文件变成了一个字一行的问题

--- ban.py
+++ ban.py
@@ -99,7 +99,7 @@ def calc_iptables_ban_rules(ips):
 def reset_rules(new_ips):
     rule_file = conf.nfResetRule % ', '.join(new_ips)
     logging.info('generator rule file: \n%s' % rule_file)
-    write_lines(conf.tmpRuleFile, rule_file)
+    write_lines(conf.tmpRuleFile, [rule_file])
     exec_cmd('/usr/sbin/nft -f %s' % conf.tmpRuleFile)
     os.remove(conf.tmpRuleFile)
 
